d600f2e7d0b6e623890c9274b83e59536fc0eabb
SAVINDUGUNATHILAKA <savindugunathilaka08@gmail.com>
2026-07-16 16:06:54 +0530

    Refactor variable initialization and update console logs for type checking

--- js/app.js
+++ js/app.js
@@ -1,19 +1,31 @@
-let name = "ss";
+
+
+
+
+
+let name = "me";
 
 let age = 18;
-name="ran";
+
+let isAfterAL = false;
+
+let customers = [];
+
+let customer = {};
+
+let salary = 30000;
 
 
 
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isAfterAL);
+console.log(typeof customers);
+console.log(typeof customer);
+console.log(typeof salary);
 
 
+let x = 10;
+let y = "10";
 
-
-
-
-console.log(name);
-console.log(age);
-alert("Hello")
-
-
-
+console.log(x===y);
