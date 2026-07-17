0f8fefed5e43100225146a35e9a03c611b74bd79
SAVINDUGUNATHILAKA <savindugunathilaka08@gmail.com>
2026-07-17 23:59:32 +0530

    Remove commented console logs for type checking and maintain code cleanliness

--- js/app.js
+++ js/app.js
@@ -17,15 +17,31 @@ let salary = 30000;
 
 
 
-console.log(typeof name);
-console.log(typeof age);
-console.log(typeof isAfterAL);
-console.log(typeof customers);
-console.log(typeof customer);
-console.log(typeof salary);
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof isAfterAL);
+// console.log(typeof customers);
+// console.log(typeof customer);
+// console.log(typeof salary);
 
 
 let x = 10;
 let y = "10";
 
-console.log(x===y);
+// console.log(x===y);
+
+
+let day = 4;
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    default:
+        console.log("Invalid day");
+}
